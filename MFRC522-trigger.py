86ddb6129dec2c7c91f1f505ea0263424edc4f6a
Stefan <stefan@layereight.de>
2018-09-06 00:43:22 +0200

    add more tags to map

--- MFRC522-trigger.py
+++ MFRC522-trigger.py
@@ -35,6 +35,36 @@ map = {
         'id': 12344,
         'name': "Mobile Phone",
         'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '13648823361': {
+        'id': 13648823361,
+        'name': "Loewe",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '13647223446': {
+        'id': 13647223446,
+        'name': "Zebra",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '1364223226177': {
+        'id': 1364223226177,
+        'name': "Giraffe",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '1364209227190': {
+        'id': 1364209227190,
+        'name': "Hyaene",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '1364215226185': {
+        'id': 1364215226185,
+        'name': "Elefant",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
+    },
+    '13648223553': {
+        'id': 13648223553,
+        'name': "Nilpferd",
+        'url': "http://localhost:3000/api/v1/commands/?cmd=playplaylist&name=SoaD"
     }
 }
 
