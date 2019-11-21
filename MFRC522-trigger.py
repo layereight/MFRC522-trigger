650c6890441bc74d4a0c0acb474c8cc0a23a5f15
Stefan <stefan@layereight.de>
2019-11-21 23:12:27 +0100

    enable polling mode when tag is detected, polling mode in turn enables detection of a tag being removed, default tag removal behaviour is to reset current_tag

--- MFRC522-trigger.py
+++ MFRC522-trigger.py
@@ -38,37 +38,47 @@ reader = pirc522.RFID()
 
 current_tag = ''
 count = 0
+polling = False
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while True:
     try:
         # wait for reader to send an interrupt
-        reader.wait_for_tag()
+        if not polling:
+            reader.wait_for_tag()
 
         count += 1
         logging.debug("Reader loop %d", count)
 
         # scan for cards
-        (error, tag_type) = reader.request()
+        if not polling:
+            (error, tag_type) = reader.request()
 
-        # on error continue and retry
-        if error:
-            continue
+            # on error continue and retry
+            if error:
+                # logging.info("error request")
+                polling = False
+                continue
 
         # get the UID of the card
         (error, uid) = reader.anticoll()
 
         # on error continue and retry
         if error:
+            # logging.info("error anticoll")
+            current_tag = ''
+            polling = False
             continue
 
         # transform UID into string representation
         tag_id = ''.join((str(x) for x in uid))
 
+        polling = True
+
         # when we're still reading the same tag
         if current_tag == tag_id:
             # don't busy wait while there's a rfid tag near the reader
-            time.sleep(0.2)
+            time.sleep(0.1)
             continue
 
         current_tag = tag_id
