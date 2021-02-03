2fde73c6b9d2d368ee19e095d89125aaa152bad6
Stefan <stefan@layereight.de>
2021-02-03 22:59:06 +0100

    rename to ValidateConfigTestCase

--- test/test_config_validation.py
+++ test/test_config_validation.py
@@ -6,7 +6,7 @@ from assertpy import assert_that
 from config import validate_config
 
 
-class ResolveActionsTestCase(unittest.TestCase):
+class ValidateConfigTestCase(unittest.TestCase):
 
     def setUp(self):
         self.config = {
