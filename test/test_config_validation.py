0abe62f0c02e2196f1ece29a31965ecb42536d2e
Stefan <stefan@layereight.de>
2021-02-05 21:32:00 +0100

    add even more unit tests for config validation

--- test/test_config_validation.py
+++ test/test_config_validation.py
@@ -137,6 +137,92 @@ class ValidateConfigTestCase(unittest.TestCase):
         # then
         assert_that(result).is_false()
 
+    @staticmethod
+    def test_tag_wrong_type_onremove():
+        # given
+        config = {
+            "888": {
+                "name": "cool tag",
+                "ondetect": [],
+                "onremove": "not okay"
+            }
+        }
+
+        # when
+        result = validate_config(config)
+
+        # then
+        assert_that(result).is_false()
+
+    @staticmethod
+    def test_tag_wrong_type_onredetect():
+        # given
+        config = {
+            "999": {
+                "name": "cool tag",
+                "ondetect": [],
+                "onredetect": "not okay"
+            }
+        }
+
+        # when
+        result = validate_config(config)
+
+        # then
+        assert_that(result).is_false()
+
+    @staticmethod
+    def test_event_combination_with_onremove():
+        # given
+        config = {
+            "000": {
+                "name": "cool tag",
+                "ondetect": [],
+                "onremove": []
+            }
+        }
+
+        # when
+        result = validate_config(config)
+
+        # then
+        assert_that(result).is_true()
+
+    @staticmethod
+    def test_event_combination_with_onredetect():
+        # given
+        config = {
+            "1111": {
+                "name": "cool tag",
+                "ondetect": [],
+                "onredetect": []
+            }
+        }
+
+        # when
+        result = validate_config(config)
+
+        # then
+        assert_that(result).is_true()
+
+    @staticmethod
+    def test_event_combination_all():
+        # given
+        config = {
+            "2222": {
+                "name": "cool tag",
+                "ondetect": [],
+                "onremove": [],
+                "onredetect": []
+            }
+        }
+
+        # when
+        result = validate_config(config)
+
+        # then
+        assert_that(result).is_true()
+
 
 if "__main__" == __name__:
     unittest.main(verbosity=4)
